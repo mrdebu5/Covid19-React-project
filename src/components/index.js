fdd5f527f521433fc5a1f29c36c4d3a1e105f1a2
Devng Langhanoja <62923975+mrdebu5@users.noreply.github.com>
2020-12-01 12:57:44 +0530

    All files

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Chart } from "../components/Chart/Charts";
+export { default as CountryPicker } from "../components/CountryPicker/CountryPicker";
+export { default as Cards } from "../components/Cards/Cards";
+export { default as Footer } from "../components/partials/Footer";
---
+++ src/components/partials/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from '../partials/Footer.module.css';
+
+function Footer() {
+    return (
+        <div className={Style.footer}>
+            <h1>Developed by || Devang Langhanoja</h1>
+        </div>
+    );
+}
+
+export default Footer
